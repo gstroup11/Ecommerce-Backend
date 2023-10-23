a5e958afb5ca9c7023c1e9e1de74340adcd8bfcc
gstroup11 <gstroup11@gmail.com>
2023-10-23 16:03:02 -0400

    this commit finishes all models, with this push focusing on the product tag model and its keys. also edited some references to target the other models correctly.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,35 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection
 const sequelize = require('../config/connection');
 
+// Initialize Product model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
+//Product model and its keys
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
