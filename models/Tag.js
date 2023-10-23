7f628eeede1e951bdb657129232e9ea4f91ee1d0
gstroup11 <gstroup11@gmail.com>
2023-10-23 15:58:49 -0400

    Commit is focused on tag.js in the models folder.  finished the model and its keys.

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,23 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-
+// import our database connection
 const sequelize = require('../config/connection.js');
 
+// Initialize Product model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
+//tag model and its keys
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
